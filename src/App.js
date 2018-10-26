f6aaea9cbc74075dacdbf286a6678b441b5e5464
Jhonatan Zubieta <jhonatan.zubietahernandez@olx.com>
2018-10-26 14:56:43 -0300

    added kawaii

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import Counter from './components/Counter';
 import './App.css';
 import Form from './components/Form';
+import { Planet, Mug, SpeechBubble } from 'react-kawaii';
 
 class App extends Component {
   render() {
     return (
       <>
+        <SpeechBubble mood="happy" />
+        <Mug size={200} mood="happy" color="#FDA7DC" />
+        <Planet size={200} mood="sad" color="#FDA7DC" />
         <Counter />
         <Form title="HolAAA" />
       </>
